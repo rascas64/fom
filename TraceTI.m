a0e362bcfb1e25a04b4d4583fc3c76a16e6e1500
rascas64 <91527489+rascas64@users.noreply.github.com>
2021-12-26 20:14:18 +0100

    Add files via upload

--- TraceTI.m
+++ TraceTI.m
@@ -1,6 +1,20 @@
-function TraceTI(t, y)
-    plot(t,y);
+function TraceTI(t, y, stair, plt)
+    if stair == true
+        if ~isempty(t)
+            stairs(t, y);
+            ylim([-0.25, 1.25]); grid on;
+        else
+            stairs(y);
+            ylim([-0.25, 1.25]); grid on;
+        end 
+    end
+    if plt == true
+        if ~isempty(t)
+            plot(t, y);
+        else 
+            plot(y);
+        end
+    end
     title("Représentation temporelle du signal");
-    xlabel("Temps en secondes");
-    ylabel("Amplitude");
+    xlabel("Temps en secondes"); ylabel("Amplitude");
 end
--- main.m
+++ main.m
@@ -4,23 +4,34 @@ close all;
 %% Signal test
 fs = 40;
 t = 0:1/fs:2*pi;
-y = cos(2*pi*15*t);
+y = cos(2*pi*t);
 
 figure
 subplot(1, 2, 1);
-TraceTI(t, y);
+TraceTI(t, y, false, true);
 subplot(1, 2, 2);
 TraceFFT(t, y);
 
 %% Question 2 
 
-n = 10000; 
-t = linspace(0, n, n);
-sign = randn(1, n);
+fs = 30;
+t = 0:1/fs:1;
+n = length(t);
+sign = randi([0, 1], 1, n);
 
 figure
 subplot(1, 2, 1);
-TraceTI(t, sign);
+TraceTI(t, sign, true, false);
 subplot(1, 2, 2);
 TraceFFT(t, sign);
 
+%% Question 3
+
+[signOOK, carrier] = OOK(sign, t(length(t)), fs, 10, 5);
+figure
+subplot(3, 1, 1);
+TraceTI(t, sign, true, false);
+subplot(3, 1, 2);
+TraceTI(t, carrier, false, true);
+subplot(3, 1, 3);
+TraceTI([], signOOK, false, true);
