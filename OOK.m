d7cbd0be0367f26024823603d189e3f808a14925
rascas64 <91527489+rascas64@users.noreply.github.com>
2021-12-27 16:41:25 +0100

    Add files via upload

---
+++ OOK.m
@@ -0,0 +1,15 @@
+function [sign_OOK, carrier] = OOK(bin, ts, fs, fc, Amax)
+    t = 0:1/fs:ts;
+    carrier = Amax * cos(fc * t);
+    cp = []; mod = [];
+    for n = 1:length(bin)
+        if bin(n) == 0
+            m = zeros(1, length(t));
+        else
+            m = ones(1, length(t));
+        end
+        cp = [cp carrier];
+        mod = [mod m];
+    end
+    sign_OOK = cp.*mod;
+end
